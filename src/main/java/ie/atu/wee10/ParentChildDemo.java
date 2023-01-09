13316e79aa15ecea67b5a3b8680ccd87caff561c
Patrick <g00409592@atu.ie>
2023-01-09 18:33:11 +0000

    Created ParentClass, ChildClass and renamed main to ParentClass. Used super to print Hi There in ChildClass.

---
+++ src/main/java/ie/atu/wee10/ParentChildDemo.java
@@ -0,0 +1,7 @@
+package ie.atu.wee10;
+
+public class ParentChildDemo {
+    public static void main(String[] args) {
+        ChildClass myParent = new ChildClass();
+    }
+}
---
+++ src/main/java/ie/atu/wee10/ParentClass.java
@@ -0,0 +1,12 @@
+package ie.atu.wee10;
+
+public class ParentClass {
+
+    public ParentClass() {
+        System.out.println("You are calling the parent constructor");
+    }
+
+    public ParentClass(String message) {
+        System.out.println(message);
+    }
+}
